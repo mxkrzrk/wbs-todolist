707b6034bfc435e1e883bd7cedbca8f454ade192
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-01-27 17:52:28 +0100

    feat: Create the TodoList class

---
+++ app-classes.js
@@ -0,0 +1,176 @@
+// Todos list
+/* 
+  {
+    id: new Date().getTime(),
+    task: '', 
+    done: false,
+  }
+*/
+
+class ToDoList {
+  constructor(submitForm, userInput) {
+    this._toDoList = [];
+    this._submitForm = document.getElementById(submitForm);
+    this._userInput = document.getElementById(userInput);
+    this.init();
+  }
+
+  init() {
+    this._submitForm.addEventListener('submit', this.createTodoHandle);
+  }
+
+  // Create a new todo
+  createTodoHandle(e) {
+    e.preventDefault();
+    console.log('hello')
+    // Store the user input
+    const userInputValue = this._userInput.value;
+    // Guard for the user input
+    if (!userInputValue) return;
+    // Create todo
+    const todo = {
+      id: new Date().getTime(),
+      task: userInputValue,
+      done: false,
+    };
+    // Store the todo in the todos list
+    this._toDoList.push(todo);
+    // Clean the form
+    document.forms[0].reset();
+    // Display todo list
+    //displayTodoList();
+  }
+}
+
+new ToDoList('taskInputForm', 'taskInput');
+
+// Listen the submit button of input field
+// const submitForm = document.getElementById('taskInputForm');
+// submitForm.addEventListener('submit', createTodoHandle);
+
+// Listen todos list
+// const todoListElement = document.getElementById('todoList');
+// todoListElement.onclick = (e) => {
+//   if (e.target.nodeName === 'P') editTodoHandle(e);
+//   if (e.target.parentNode.nodeName === 'BUTTON') deleteTodoHandle(e);
+//   if (e.target.parentNode.nodeName === 'DIV') markDoneTaskHandle(e);
+// };
+
+// // CRUD Operations
+// // Create a new todo
+// // function createTodoHandle(e) {
+// //   e.preventDefault();
+// //   // Store the user input
+// //   const userInputValue = document.getElementById('taskInput').value;
+// //   // Guard for the user input
+// //   if (!userInputValue) return;
+// //   // Create todo
+// //   const todo = {
+// //     id: new Date().getTime(),
+// //     task: userInputValue,
+// //     done: false,
+// //   };
+// //   // Store the todo in the todos list
+// //   toDoList.push(todo);
+// //   // Clean the form
+// //   document.forms[0].reset();
+// //   // Display todo list
+// //   displayTodoList();
+// // }
+
+// // Display (Read) todos list
+// function displayTodoList() {
+//   const todoListElement = document.getElementById('todoList');
+//   const todoListNotDone = document.getElementById('todoListNotDone');
+//   const todoListDone = document.getElementById('todoListDone');
+//   // Clear the lists previews
+//   todoListNotDone.innerHTML = '';
+//   todoListDone.innerHTML = '';
+//   toDoList.length > 0
+//     ? todoListElement.classList.add('todolist')
+//     : todoListElement.classList.remove('todolist');
+//   // Create the li for each todo in the object
+//   toDoList.forEach((todo, index) => {
+//     let liElement;
+//     if (todo.done) {
+//       liElement = `
+//       <li id="${todo.id}" class="d-flex justify-content-between align-items-center">
+//         <div><i class="fas fa-check-circle"></i></div>
+//         <p class="mb-0 w-100 mx-2 text-decoration-line-through">${todo.task}</p>
+//         <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+//       </li>
+//       `;
+//       todoListDone.insertAdjacentHTML('beforeend', liElement);
+//       if (todoListDone.children.length === 1) {
+//         const titleElement =
+//           '<div class="todolist-title d-flex justify-content-start align-items-center"><i class="fas fa-tasks"></i><h3>Completed</h3></div>';
+//         todoListDone.insertAdjacentHTML('afterbegin', titleElement);
+//       }
+//     } else {
+//       liElement = `
+//       <li id="${todo.id}" class="d-flex justify-content-between align-items-center">
+//         <div><i class="far fa-circle"></i></div>
+//         <p class="mb-0 w-100 mx-2">${todo.task}</p>
+//         <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+//       </li>
+//       `;
+//       todoListNotDone.insertAdjacentHTML('beforeend', liElement);
+//     }
+//   });
+// }
+
+// // Edit (Update) a todo
+// function editTodoHandle(e) {
+//   // Target the todo to update
+//   const todoEditableElement = e.target;
+//   // Turn on to edit the content
+//   todoEditableElement.setAttribute('contenteditable', 'true');
+//   // Listen when user press enter or blur the target
+//   todoEditableElement.addEventListener('blur', saveUpdateTodoHandle);
+//   todoEditableElement.onkeydown = (e) => {
+//     if (e.key === 'Enter') {
+//       // Avoid run two times the callback function
+//       todoEditableElement.removeEventListener('blur', saveUpdateTodoHandle);
+//       saveUpdateTodoHandle(e);
+//     }
+//   };
+// }
+
+// // Save (Update) the todo updated
+// function saveUpdateTodoHandle(e) {
+//   // Target the todo to update
+//   const todoEditableElement = e.target;
+//   // Turn off to edit the content
+//   todoEditableElement.removeAttribute('contenteditable', 'false');
+//   // Filter todo in the list of todos
+//   const todoFiltered = toDoList.filter(
+//     (todo) => todo.id === parseInt(e.target.parentNode.id)
+//   );
+//   // Store user input value updated
+//   todoFiltered[0].task = todoEditableElement.innerText;
+// }
+
+// // Delete todo
+// function deleteTodoHandle(e) {
+//   // Filter todo in the list of todos
+//   const todoFiltered = toDoList.filter(
+//     (todo) => todo.id !== parseInt(e.target.parentNode.parentNode.id)
+//   );
+//   // Update ToDo list
+//   toDoList = todoFiltered;
+//   // Update UI
+//   displayTodoList();
+// }
+
+// // Mark done the task
+// function markDoneTaskHandle(e) {
+//   // Retrieve task ID
+//   const todoDone = toDoList.map((todo) =>
+//     todo.id === parseInt(e.target.parentNode.parentNode.id)
+//       ? { ...todo, done: !todo.done }
+//       : { ...todo }
+//   );
+//   toDoList = todoDone;
+//   // Update UI
+//   displayTodoList();
+// }
